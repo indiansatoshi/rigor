de01e82820948042455c2c3fcf2644249618025a
indiansatoshi <reganayyappan@gmail.com>
2025-11-23 20:23:00 +0000

    improvement22

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -2,7 +2,5 @@
 export { PageSection } from "./page-section";
 export { PageTabs, type PageTab } from "./page-tabs";
 export { PageWrapper } from "./page-wrapper";
-export { SideNav } from "./side-nav";
 export { TopNav } from "./top-nav";
 export { PageBreadcrumbs } from "./page-breadcrumbs";
-export { SidebarProvider, useSidebar } from "./sidebar-context";
